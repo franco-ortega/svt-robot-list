cbbd764cd33041b45f1ae22a8cc5890685981b66
Franco Ortega <francopdx@gmail.com>
2022-10-07 11:13:35 -0700

    create initial Row component

---
+++ components/table/Row.jsx
@@ -0,0 +1,12 @@
+const Row = () => {
+  return (
+    <div className='row'>
+      <div className='col'>ID</div>
+      <div className='col'>Battery Level</div>
+      <div className='col'>Y</div>
+      <div className='col'>X</div>
+    </div>
+  );
+};
+
+export default Row;
